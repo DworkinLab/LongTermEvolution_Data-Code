480f4ac12d7919a37ecb4d32abde5b0d7bc2f0ba
Paul Knoops <knoopsp@mcmaster.ca>
2017-02-06 17:29:45 -0500

    New R project

---
+++ AP_data_R_script.R
@@ -0,0 +1,4 @@
+# Data from Abhijna of the long term populations and their courtship and copulation data
+
+AP_evolved_data <- read.csv("AP_EvolvedPopCourtshipCopulation_2014.csv", h=T)
+head(AP_evolved_data)
