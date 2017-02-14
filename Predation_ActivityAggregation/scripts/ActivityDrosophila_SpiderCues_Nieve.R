f40cf6771a234896e7750ec2a15406b3e848a489
Paul Knoops <knoopsp@mcmaster.ca>
2017-02-14 13:00:58 -0500

    1st plots and model

--- Predation_ActivityAggregation/scripts/ActivityDrosophila_SpiderCues_Nieve.R
+++ Predation_ActivityAggregation/scripts/ActivityDrosophila_SpiderCues_Nieve.R
@@ -20,49 +20,41 @@ Mon2 <- read.table("DrosophilaActivity_Spider_cues_Monitor2_May2016.txt")
 Mon1$V43 <- 1
 Mon2$V43 <- 2
 
-Act_Data <- rbind(Mon1, Mon2)
+colnames(Mon1) <- c("bin", "date", "time", "signal", "unknown1", "unknown2", "unknown3", "unknown4", "unknown5", "lightON",'vial1', 'vial2', 'vial3', 'vial4', 'vial5', 'vial6', 'vial7', 'vial8', 'vial9', 'vial10', 'vial11', 'vial12', 'vial13', 'vial14', 'vial15', 'vial16', 'vial17', 'vial18', 'vial19', 'vial20', 'vial21', 'vial22', 'vial23', 'vial24', 'vial25', 'vial26', 'vial27', 'vial28', 'vial29', 'vial30', 'vial31', 'vial32', 'monitor')
 
-colnames(Act_Data) <- c("bin", "date", "time", "signal", "unknown1", "unknown2", "unknown3", "unknown4", "unknown5", "lightON",'vial1', 'vial2', 'vial3', 'vial4', 'vial5', 'vial6', 'vial7', 'vial8', 'vial9', 'vial10', 'vial11', 'vial12', 'vial13', 'vial14', 'vial15', 'vial16', 'vial17', 'vial18', 'vial19', 'vial20', 'vial21', 'vial22', 'vial23', 'vial24', 'vial25', 'vial26', 'vial27', 'vial28', 'vial29', 'vial30', 'vial31', 'vial32', 'monitor')
-
-head(Act_Data)
+colnames(Mon2) <- c("bin", "date", "time", "signal", "unknown1", "unknown2", "unknown3", "unknown4", "unknown5", "lightON",'vial1', 'vial2', 'vial3', 'vial4', 'vial5', 'vial6', 'vial7', 'vial8', 'vial9', 'vial10', 'vial11', 'vial12', 'vial13', 'vial14', 'vial15', 'vial16', 'vial17', 'vial18', 'vial19', 'vial20', 'vial21', 'vial22', 'vial23', 'vial24', 'vial25', 'vial26', 'vial27', 'vial28', 'vial29', 'vial30', 'vial31', 'vial32', 'monitor')
 
 #Remove unknowns
-Act_Data2 <- Act_Data[,-c(5:9)]
-
+Mon1 <- Mon1[,-c(5:9)]
+Mon2 <- Mon2[,-c(5:9)]
 #Remove unneeded vials (i.e vials 9 - 24)
 #14 - 29
-Act_Data2 <- Act_Data2[,-c(14:29)]
+Mon1 <- Mon1[,-c(14:29)]
+Mon2 <- Mon2[,-c(14:29)]
 
-head(Act_Data2)
 #Change Day format
-#Act_Data2 <- Act_Data2 %>% 
-#  separate(date, c("Month","Day","Year"), "/")
-#head(Act_Data2)
-Act_Data2$datetime <- as.POSIXct(paste(Act_Data2$date, Act_Data2$time), format="%m/%d/%y %H:%M:%S")
-head(Act_Data2)
+Mon1$datetime <- as.POSIXct(paste(Mon1$date, Mon1$time), format="%m/%d/%y %H:%M:%S")
+Mon2$datetime <- as.POSIXct(paste(Mon2$date, Mon2$time), format="%m/%d/%y %H:%M:%S")
 
 
 #Reshape to long
 #Episodic_long <- gather(Data_subset, Population, Allele_Freq , ConR1_115:AncestorR1_0, factor_key=TRUE)
-Act_long <- gather(Act_Data2, Vial, Activity_counts, vial1:vial32, factor_key = FALSE)
-head(Act_long)
+Mon1_long <- gather(Mon1, Vial, Activity_counts, vial1:vial32, factor_key = FALSE)
+Mon2_long <- gather(Mon2, Vial, Activity_counts, vial1:vial32, factor_key = FALSE)
+
 
 # To get minute
-Act_long$minute <- as.numeric(strftime(Act_long$datetime, format ="%M"))
+Mon1_long$minute <- as.numeric(strftime(Mon1_long$datetime, format ="%M"))
+Mon2_long$minute <- as.numeric(strftime(Mon2_long$datetime, format ="%M"))
 # to get hour
-Act_long$hour <- as.numeric(strftime(Act_long$datetime, format ="%H"))
+Mon1_long$hour <- as.numeric(strftime(Mon1_long$datetime, format ="%H"))
+Mon2_long$hour <- as.numeric(strftime(Mon2_long$datetime, format ="%H"))
 # to get day
-Act_long$day <- as.numeric(strftime(Act_long$datetime, format = "%d"))
+Mon1_long$day <- as.numeric(strftime(Mon1_long$datetime, format = "%d"))
+Mon2_long$day <- as.numeric(strftime(Mon2_long$datetime, format = "%d"))
 
-head(Act_long)
-
-
-#day.vial -- is it needed or was it just to merge
-#Act_long$day.vial <- interaction(Act_long$day, Act_long$vial)
-
-#Removed vials in middle above
-
-#Note; one column (when dark == purple on excel) has a jump in activity of 1 for that time minute--> both monitors == bin 595
+head(Mon1_long)
+head(Mon2_long)
 
 
 # monitor 1: 
@@ -70,8 +62,29 @@ head(Act_long)
 # monitor 2:
 #vials 1-8 = predator ------ vials 25- 32 == Control
 
-#Act_long$Treatment <- "Spider"
+Mon1_long$Treatment <- ifelse(Mon1_long$Vial == "vial1", "Control", ifelse (Mon1_long$Vial == "vial2", "Control", ifelse(Mon1_long$Vial == "vial3", "Control", ifelse(Mon1_long$Vial == "vial4", "Control", ifelse(Mon1_long$Vial == "vial5", "Control", ifelse(Mon1_long$Vial == "vial6", "Control", ifelse(Mon1_long$Vial == "vial7", "Control", ifelse(Mon1_long$Vial == "vial8", "Control","Spider"))))))))
 
-#Act_long$Treatment <- ifelse(Act_long$Vial == "vial1" && Act_long$monitor =="1", "Control", ifelse(Act_long$Vial == "vial2" && Act_long$monitor =="1", "Control", ifelse(Act_long$Vial == "vial3" && Act_long$monitor =="1", "Control", ifelse(Act_long$Vial == "vial4" && Act_long$monitor =="1", "Control", ifelse(Act_long$Vial == "vial5" && Act_long$monitor =="1", "Control", ifelse(Act_long$Vial == "vial6" && Act_long$monitor =="1", "Control", ifelse(Act_long$Vial == "vial7" && Act_long$monitor =="1", "Control", ifelse(Act_long$Vial == "vial8" && Act_long$monitor =="1", "Control", ifelse(Act_long$Vial == "vial25" && Act_long$monitor =="2", "Control", ifelse(Act_long$Vial == "vial26" && Act_long$monitor =="2", "Control", ifelse(Act_long$Vial == "vial27" && Act_long$monitor =="2", "Control",ifelse(Act_long$Vial == "vial28" && Act_long$monitor =="2", "Control", ifelse(Act_long$Vial == "vial29" && Act_long$monitor =="2", "Control", ifelse(Act_long$Vial == "vial30" && Act_long$monitor =="2", "Control", ifelse(Act_long$Vial == "vial31" && Act_long$monitor =="2", "Control", ifelse(Act_long$Vial == "vial32" && Act_long$monitor =="2", "Control", "Spider"))))))))))))))))
+Mon2_long$Treatment <- ifelse(Mon2_long$Vial == "vial1", "Spider", ifelse (Mon2_long$Vial == "vial2", "Spider", ifelse(Mon2_long$Vial == "vial3", "Spider", ifelse(Mon2_long$Vial == "vial4", "Spider", ifelse(Mon2_long$Vial == "vial5", "Spider", ifelse(Mon2_long$Vial == "vial6", "Spider", ifelse(Mon2_long$Vial == "vial7", "Spider", ifelse(Mon2_long$Vial == "vial8", "Spider","Control"))))))))
 
-#Simpler would be to put this before rbind --
+Act_long <- rbind(Mon1_long, Mon2_long)
+
+#Make individual codes for each ??? -- look at old code?
+#Note; one column (when dark == purple on excel) has a jump in activity of 1 for that time minute--> both monitors == bin 595
+
+#Quick look at daily activity
+day_act <- Act_long %>%
+  group_by(Treatment, monitor, Vial, day) %>%
+  summarise(mean_activity=mean(Activity_counts))
+
+
+lattice::bwplot(mean_activity ~ as.factor(Treatment), data=day_act)
+lattice::bwplot(mean_activity ~ as.factor(monitor):as.factor(day), data=day_act)
+
+p1 <- ggplot(data = day_act, aes(x = Treatment, y = mean_activity))
+p1+geom_boxplot()
+
+#copied model
+#day_act.lmer <- lmer(mean_activity ~ Treatment + monitor +  + (1|day), data=day_act)
+#car::Anova(day_act.lmer)
+#summary(day_act.lmer)
+#confint(day_act.lmer)
