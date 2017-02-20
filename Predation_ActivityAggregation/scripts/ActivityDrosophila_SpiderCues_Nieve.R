f9c7319ceb5052df3fa54ef7a9f7613521d06682
Paul Knoops <knoopsp@mcmaster.ca>
2017-02-20 10:42:37 -0500

    hourly activity plot and many other updates

--- Predation_ActivityAggregation/scripts/ActivityDrosophila_SpiderCues_Nieve.R
+++ Predation_ActivityAggregation/scripts/ActivityDrosophila_SpiderCues_Nieve.R
@@ -6,6 +6,7 @@
 
 #Libraries
 library(lme4)
+library(nlme)
 library(tidyr)
 library(dplyr)
 library(ggplot2)
@@ -75,16 +76,20 @@ Act_long <- rbind(Mon1_long, Mon2_long)
 #Note; one column (when dark == purple on excel) has a jump in activity of 1 for that time minute--> both monitors == bin 595
 
 #Forgot this:
+head(Act_long)
 Act_long$monitor <- as.factor(Act_long$monitor)
-
+Act_long$Treatment <- as.factor(Act_long$Treatment)
+Act_long$day <- as.factor(Act_long$day)
+Act_long$Vial <- as.factor(Act_long$Vial)
 #Quick look at daily activity
+
 day_act <- Act_long %>%
   group_by(Treatment, monitor, Vial, day) %>%
   summarise(mean_activity=mean(Activity_counts))
 
 
-lattice::bwplot(mean_activity ~ as.factor(Treatment), data=day_act)
-lattice::bwplot(mean_activity ~ as.factor(monitor):as.factor(day), data=day_act)
+#lattice::bwplot(mean_activity ~ as.factor(Treatment), data=day_act)
+#lattice::bwplot(mean_activity ~ as.factor(monitor):as.factor(day), data=day_act)
 
 #My own plots (ggplot which I know better)
 
@@ -97,12 +102,124 @@ plo2+geom_boxplot()
 plo3 <- ggplot(data = day_act, aes(Treatment, mean_activity))
 plo3+geom_boxplot()
 
-day_act$Treatment <- as.factor(day_act$Treatment)
+#Moved above to Act_long
+#day_act$Treatment <- as.factor(day_act$Treatment)
+#day_act$day <- as.factor(day_act$day)
+#day_act$monitor <- as.factor(day_act$monitor)
+
+
 #copied model from Ian Script (June2015)
 
 #changed some: look at old (diff)
-day_act.lmer <- lmer(mean_activity ~ Treatment + day + monitor + (1|day), data=day_act)
+head(day_act)
+day_act.lmer <- lmer(mean_activity ~ Treatment + day + Vial + monitor + (1|day), data=day_act)
 car::Anova(day_act.lmer)
 summary(day_act.lmer)
 confint(day_act.lmer)
 plot(allEffects(day_act.lmer))
+
+head(Act_long)
+#All effects:
+#Act_mod <- lmer(Activity_counts ~ 1 + Treatment + date + Vial + monitor + (1|date), data = Act_long)
+#plot(allEffects(Act_mod))
+
+#Act_all <- Act_long %>%
+#  group_by(Treatment, Vial, monitor, day, lightON, time) %>%
+#  summarise(meanActivity = mean(Activity_counts))
+#act_all_mod <- lmer(meanActivity ~ 1 + Treatment + day + Vial + monitor + lightON + time + (1|day), data = Act_all)
+
+
+#Hourly Average:
+head(Act_long)
+act_hour <- Act_long %>%
+  group_by(Treatment, Vial, monitor, day, hour) %>%
+  summarise(activity_counts = sum(Activity_counts))
+
+#Ian script calls for individuals (did not need above but needed for auto correlation?)
+
+act_hour$individual <- with(act_hour, interaction(day, Vial, monitor, drop=FALSE))
+
+#Ians Boxplot method
+with(act_hour, boxplot(activity_counts ~ hour))
+
+#My Boxplot
+act_hour$hour <- as.factor(act_hour$hour)
+hour_gg <- ggplot(act_hour, aes(x=hour, y= activity_counts, colour=Treatment))
+hour_gg + geom_boxplot()
+
+
+##Possibly need to shift times to start of experiment?
+#head(Mon1)
+#head(Mon2)
+Mon2$time
+#Start time = first reading == 12:00
+#shift times by 12 hours:
+act_hour$hour <- as.numeric(act_hour$hour)
+act_hour$hour_shift <- ifelse(act_hour$hour >= 12, (act_hour$hour - 12), (act_hour$hour +12))
+
+#Ian Plot
+with(act_hour, boxplot(activity_counts ~ hour_shift))
+#My plot
+act_hour$hour_shift
+act_hour$hour_shift <- as.factor(act_hour$hour_shift)
+hourshift_gg <- ggplot(act_hour, aes(x=hour_shift, y= activity_counts, colour=Treatment))
+hourshift_gg + geom_boxplot()
+
+
+head(act_hour)
+#Light or Dark -- light on at ~10am, off at 22:00
+#Do before hour shifting? -- don't use hour shift and it works
+#Actually off at 22:05 and on at 10:05???????????????????????
+
+act_hour$light <- with(act_hour, ifelse(hour >= 10 & hour < 22, "light", "dark"))
+
+hour.mod <- lm(activity_counts ~ Treatment + hour_shift + monitor + day,data=act_hour)
+summary(hour.mod)
+pacf(resid(hour.mod))
+
+#Auto-correlation:
+#act_hour$individual <- as.factor(act_hour$individual)
+#act_hour$hour_shift <- as.factor(act_hour$hour_shift)
+
+#??gls
+#generalized least squares
+
+correl_mod <- gls(activity_counts ~ Treatment + hour_shift + monitor + day, correlation = corAR1(form = ~ 1|hour_shift), data=act_hour)
+anova(correl_mod)
+summary(correl_mod)
+acf(resid(correl_mod))
+
+#With light
+act_cor_light_mod <- gls(activity_counts ~ Treatment + light + light:Treatment +  hour_shift + monitor + day, correlation = corAR1(form =~1|hour_shift), control = list(singular.ok = TRUE), data=act_hour)
+summary(act_cor_light_mod)
+confint(act_cor_light_mod)
+
+
+#Same Plot:::::
+act_hour$hour_shift <- as.numeric(act_hour$hour_shift)
+
+
+with(act_hour, plot(activity_counts ~ jitter(hour_shift), pch=20, cex=0.1))
+lines(smooth.spline(y=act_hour$activity_counts, x = act_hour$hour_shift))
+lines(lowess(act_hour$activity_counts ~ act_hour$hour_shift, f=0.1), col="red")
+
+
+with(act_hour[act_hour$Treatment=="Control",], 
+     plot(activity_counts ~ jitter(hour_shift, factor=1.3), pch=20, cex=0.2,
+          xlab ="hours after initiation", ylab = "hourly activity",
+          main = "temporal patterns of activity ",
+          ylim=c(0,400)))
+
+with(act_hour[act_hour$Treatment=="Control",], lines(smooth.spline(y=activity_counts, x = hour_shift),lwd=2))
+
+with(act_hour[act_hour$Treatment=="Spider",], points(activity_counts ~ jitter(hour_shift, factor=1.3), pch=20, cex=0.2, col="red"))
+with(act_hour[act_hour$Treatment=="Spider",], lines(smooth.spline(y=activity_counts, x = hour_shift), col="red", lwd=2))
+
+
+legend(x=15, y=400, legend=c("Control", "Spider"), pch=20, col=c(1, "red"))
+
+
+#Change when lights went on.. 10:00 am, start was noon (lights already on..., off at 10 at night -- shift == off at "10" after 0, on at 22)
+rect(xleft=0, xright=10, ybottom = 0, ytop = 830, col="#ffff0032", border=NA)
+rect(xleft=22, xright=24, ybottom = 0, ytop = 830, col="#ffff0032", border=NA)
+
