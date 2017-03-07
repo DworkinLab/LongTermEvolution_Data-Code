0dadd18309d474ca243bd440557539df71313675
Paul Knoops <Paul Knoops>
2017-03-07 09:58:26 -0500

    One windows machine and Script for packages

---
+++ Predation_ActivityAggregation/scripts/Packages_source_file.R
@@ -0,0 +1,17 @@
+#Packages Script
+
+#install.packages("xlsx")
+#install.packages("lme4")
+#install.packages("nlme")
+#install.packages("tidyr")
+#install.packages("dplyr")
+#install.packages("ggplot2")
+#install.packages("effects")
+
+library(xlsx)
+library(lme4)
+library(nlme)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(effects)
