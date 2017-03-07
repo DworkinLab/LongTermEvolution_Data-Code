a9969e88407e159cff876260b99daaee38e39d75
Paul Knoops <Knoops93@gmail.com>
2017-03-07 11:27:56 -0500

    commit

--- Predation_ActivityAggregation/scripts/ActivityDrosophila_ComplexCues_EXP2.R
+++ Predation_ActivityAggregation/scripts/ActivityDrosophila_ComplexCues_EXP2.R
@@ -1,11 +1,12 @@
 ##   From Complex Cues Data Directory README.md: In Exp 2, flies had 2 types of cues marked in the top row: spiders or crickets.
 
 
-#Try reading xlsx rather than online convert to .txt
-#install.packages("xlsx")
-library(xlsx)
-#res <- read.xlsx(file, 2)
+#Setwd to scripts folder
+source("Packages_source_file.R")
 
+#Try reading xlsx rather than online convert to .txt
+#res <- read.xlsx(file, 2)
+#FOR mac only...
 ComExp2M1 <- read.xlsx("~/Bioinformatics/Long_Term_Data/Data_LongTermPopulationExperiments_Git/Predation_ActivityAggregation/data/Activity_Drosophila_ComplexCues_June17_2016/Exp 2 spider vs cricket cues M1.xlsx", 1)
 
 #Works, but date and time are different set up. need to fix???
